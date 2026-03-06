98858eca1b3649abbc0bccf10f73f4de9cae6bc7
Luis Lopez Villar <llvillar@gmail.com>
2026-03-06 10:54:30 +0100

    Versión parcialmente documentada con generacion doc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.tienda"},{"l":"com.tienda.modelo"},{"l":"com.tienda.pagos"},{"l":"com.tienda.servicio"}];updateSearchResults();
